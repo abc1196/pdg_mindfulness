4ea5f90e6478733de21702942701c11144c9d796
Nicolas <nikoremi97@hotmail.com>
2018-09-19 12:31:13 -0500

    Update Home with BottomNavigationView

--- app/src/main/java/mindfulness/pdg_mindfulness/PSTActivity.java
+++ app/src/main/java/mindfulness/pdg_mindfulness/PSTActivity.java
@@ -1,7 +1,26 @@
 package mindfulness.pdg_mindfulness;
 
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 
-public class PSTActivity extends AppCompatActivity {
+public class PSTActivity extends AppCompatActivity implements NavigationHost {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pst);
+    }
+    @Override
+    public void navigateTo(Fragment fragment, boolean addToBackstack) {
+        FragmentTransaction transaction =
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.container, fragment);
 
+        if (addToBackstack) {
+            transaction.addToBackStack(null);
+        }
+        transaction.commit();
+    }
 }
