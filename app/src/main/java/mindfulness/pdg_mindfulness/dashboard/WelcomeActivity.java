e07834af89778de8f8adc999deb1e6ed20bd2377
abc1196 <alejob_11@hotmail.com>
2018-10-07 22:31:03 -0500

    Restructured project

---
+++ app/src/main/java/mindfulness/pdg_mindfulness/dashboard/WelcomeActivity.java
@@ -0,0 +1,15 @@
+package mindfulness.pdg_mindfulness.dashboard;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import mindfulness.pdg_mindfulness.R;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+}
